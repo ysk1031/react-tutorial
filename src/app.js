11e89ff650cc546c42fe3cca75ec07b785a76c82
ysk1031 <yusukeaono.1031@gmail.com>
2016-05-07 16:31:48 +0900

    Tutorial 4

--- src/app.js
+++ src/app.js
@@ -33,6 +33,19 @@ const CommentForm = React.createClass({
   }
 });
 
+const Comment = React.createClass({
+  render() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    )
+  }
+});
+
 render(
   <CommentBox />,
   document.getElementById('container')
