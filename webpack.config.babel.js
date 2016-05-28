17686caf63deca3e6781a65f640ff3c92c256f0d
ysk1031 <yusukeaono.1031@gmail.com>
2016-05-28 17:51:59 +0900

    Update file directory

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,7 +1,7 @@
 import path from 'path';
 
-const src = path.resolve(__dirname, 'src');
-const dist = path.resolve(__dirname, 'dist');
+const src = path.resolve(__dirname, 'public/src');
+const dist = path.resolve(__dirname, 'public/dist');
 
 export default {
   entry: `${src}/app.js`,
