644b7fb51f73bd4d2b48a23a2589f6faefd7723b
ysk1031 <yusukeaono.1031@gmail.com>
2016-06-04 17:03:55 +0900

    Create actions

---
+++ public/src/actions/index.js
@@ -0,0 +1,13 @@
+export const addComment = (comment) => {
+  return {
+    type: "ADD_COMMENT",
+    comment
+  };
+};
+
+export const setComments = (comments) => {
+  return {
+    type: "SET_COMMENTS",
+    comments
+  };
+};
