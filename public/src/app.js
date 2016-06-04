45a9d4f8d0354885fb0ddce0ce1ab298353f0472
ysk1031 <yusukeaono.1031@gmail.com>
2016-06-04 17:29:26 +0900

    Create store

--- public/src/app.js
+++ public/src/app.js
@@ -2,7 +2,15 @@ import React from 'react';
 import { render } from 'react-dom';
 import CommentBox from './components/CommentBox';
 
+import { createStore } from 'redux';
+import commentReducer from './reducers';
+import { Provider } from 'react-redux';
+
+let store = createStore(commentReducer);
+
 render(
-  <CommentBox url="http://localhost:4567/api/comments" pollInterval={2000} />,
-  document.getElementById('container')
+  <Provider store={store}>
+    <CommentBox url="http://localhost:4567/api/comments" pollInterval={2000} />,
+    document.getElementById('container')
+  </Provider>
 );
